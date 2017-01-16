e1bce9d29f8c2643575a75d07a77faa87b276ab6
bridgetree <rahultach@gmail.com>
2017-01-16 12:55:52 -0500

    Create class_fraction.py

---
+++ class_fraction.py
@@ -0,0 +1,57 @@
+# A class implementing fractions
+
+class Fraction:
+    def __init__(self, top, bottom):
+        self.num = top
+        self.den = bottom
+    def show(self):
+        print self.num, "/", self.den
+    def __str__(self):
+        return str(self.num)+"/"+str(self.den)
+    def __add__(self, otherfraction):
+        newnum = self.num * otherfraction.den + self.den * otherfraction.num
+        newden = self.den * otherfraction.den
+        common = gcd(newnum, newden)
+        return Fraction(newnum // common, newden // common)
+    def __sub__(self, otherfraction):
+        newnum = self.num * otherfraction.den - self.den * otherfraction.num
+        newden = self.den * otherfraction.den
+        common = gcd(newnum, newden)
+        return Fraction(newnum // common, newden // common)
+    def __mul__(self, other):
+        newnum = self.num * other.num
+        newden = self.den * other.den
+        common = gcd(newnum, newden)
+        return Fraction(newnum // common, newden // common)
+    def __div__(self, other):
+        newnum = self.num * other.den
+        newden = self.den * other.num
+        common = gcd(newnum, newden)
+        return Fraction(newnum // common, newden // common)
+    def __eq__(self, other):
+        firstnum = self.num * other.den
+        secondnum = other.num * self.den
+        return firstnum == secondnum
+    def __lt__(self, other):
+        firstnum = self.num * other.den
+        secondnum = other.num * self.den
+        return firstnum < secondnum
+    def __gt__(self, other):
+        firstnum = self.num * other.den
+        secondnum = other.num * self.den
+        return firstnum > secondnum
+       
+# Find greatest common divisor
+
+def gcd(m ,n):
+    while m % n != 0:
+        oldm = m
+        oldn = n
+        
+        m = oldn
+        n = oldm % oldn
+        
+    return n
+
+
+        
