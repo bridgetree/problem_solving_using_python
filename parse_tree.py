24286cf869c5e3cb94b7d9a4506cf3a4882ad6e2
bridgetree <rahultach@gmail.com>
2017-01-21 21:15:37 -0500

    Update parse_tree.py

--- parse_tree.py
+++ parse_tree.py
@@ -28,3 +28,16 @@ def buildParseTree(fpexp):
     return eTree
 
 pt = buildParseTree("( ( 10 + 5 ) * 3 )")
+
+def evaluate(parseTree):
+    opers = {'+':operator.add, '-':operator.sub, '*':operator.mul, '/':operator.truediv}
+
+    leftC = parseTree.getLeftChild()
+    rightC = parseTree.getRightChild()
+
+    if leftC and rightC:
+        fn = opers[parseTree.getRootVal()]
+        return fn(evaluate(leftC),evaluate(rightC))
+    else:
+        return parseTree.getRootVal()
+
