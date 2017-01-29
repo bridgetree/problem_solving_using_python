bee38a5a00328d5969058959c56cd1d3e844142d
bridgetree <rahultach@gmail.com>
2017-01-29 06:28:46 -0500

    Update and rename binary_search.py to 12_binary_search.py

--- 12_binary_search.py
+++ 12_binary_search.py
@@ -14,4 +14,19 @@ def binarySearch(alist, item):
                 first = midpoint+1
 	
     return found
+
+# Recursion
+
+def binarySearch(alist, item):
+    if len(alist) == 0:
+        return False
+    else:
+        midpoint = len(alist)//2
+        if alist[midpoint]==item:
+          return True
+        else:
+          if item<alist[midpoint]:
+            return binarySearch(alist[:midpoint],item)
+          else:
+            return binarySearch(alist[midpoint+1:],item)
 	
