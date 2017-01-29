60d771b0c3d5788fb1fc6039f9adcf2ac2bc73cb
bridgetree <rahultach@gmail.com>
2017-01-29 13:28:15 -0500

    Update palindrome_check_recursion.py

--- palindrome_check_recursion.py
+++ palindrome_check_recursion.py
@@ -2,7 +2,7 @@ def string_reverse(mystr):
     if len(mystr) == 1:
         return mystr[0]
     else:
-        return mystr[-1] + recursion(mystr[:-1])
+        return mystr[-1] + string_reverse(mystr[:-1])
     
 def palindrome_check(mystr):
     if recursion(mystr) == mystr:
